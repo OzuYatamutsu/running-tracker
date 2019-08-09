72b146ca96940fdcfef94c16f37124ef92b840cf
Sean Collins <secollins@linkedin.com>
2019-08-09 08:58:15 -0700

    Alternate schema suggestion

---
+++ database/schema_v1.sql
@@ -0,0 +1,32 @@
+CREATE TABLE vitals (
+    timestamp DATETIME PRIMARY KEY NOT NULL,
+    weight_lb NUMERIC NOT NULL,
+    bp_systolic NUMERIC NOT NULL,
+    bp_diastolic NUMERIC NOT NULL,
+    heart_bpm UNSIGNED INTEGER NOT NULL,
+    notes TEXT(1024) NOT NULL DEFAULT ''
+);
+
+CREATE TABLE activity_types (
+    name TEXT PRIMARY KEY NOT NULL
+);
+
+-- Seed activity types into table
+INSERT INTO activity_types (name) VALUES
+    ('run'),
+    ('walk');
+
+CREATE TABLE activity (
+    timestamp DATETIME PRIMARY KEY NOT NULL,
+    activity_type TEXT NOT NULL,
+    distance_mi NUMERIC NOT NULL,
+    duration_min UNSIGNED INTEGER NOT NULL,
+    duration_sec UNSIGNED INTEGER NOT NULL,
+    temp_f NUMERIC,
+    notes TEXT(1024) NOT NULL DEFAULT ''
+
+    FOREIGN KEY (activity_type) REFERENCES activity_types(name)
+        ON UPDATE RESTRICT,
+        ON DELETE RESTRICT
+);
+
