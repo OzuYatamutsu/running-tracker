f6c39a35fd0e4766a7ad5f2a7c84ba96352a60a0
Sean Collins <seanc@seancotech.com>
2019-08-11 15:18:22 -0700

    Add tests for db interface

--- test/test_datapoint.py
+++ test/test_datapoint.py
@@ -2,9 +2,7 @@ from runningtracker.db.models.activity_datapoint import ActivityDatapoint
 from runningtracker.db.models.vitals_datapoint import VitalsDatapoint
 from runningtracker.db.models.vo2_datapoint import Vo2Datapoint
 from runningtracker.db.models.activity_type import ActivityType
-from runningtracker.db.models.datapoint import Datapoint
 from runningtracker.db.db_interface import _init_db
-from dataclasses import dataclass
 from unittest import TestCase
 from datetime import datetime
 from sqlite3 import connect
@@ -19,22 +17,6 @@ class TestDatapoint(TestCase):
         self.db.execute(
             datapoint.COMMIT_SQL, datapoint.to_sql_params()
         )
-
-    def test_incomplete_datapoint_subclassing_throws_error(self):
-        @dataclass
-        class BadDatapoint(Datapoint):
-            """
-            This should fail. It does not implement
-            required fields/methods
-            """
-            value: int
-
-        try:
-            bad_datapoint = BadDatapoint(value=1)  # noqa
-            assert bad_datapoint.value == 1
-            assert False
-        except TypeError:
-            assert True
 
     def test_vitals_datapoint_has_valid_sql(self):
         datapoint = VitalsDatapoint(
---
+++ test/test_db_interface.py
@@ -0,0 +1,69 @@
+from runningtracker.db.models.activity_datapoint import ActivityDatapoint
+from runningtracker.db.db_interface import commit, _get_db, DATABASE_NAME
+from runningtracker.db.models.vitals_datapoint import VitalsDatapoint
+from runningtracker.db.models.vo2_datapoint import Vo2Datapoint
+from runningtracker.db.models.activity_type import ActivityType
+from unittest import TestCase
+from datetime import datetime
+from os import unlink
+
+
+class TestDbInterface(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_can_commit_vitals_datapoint(self):
+        commit(VitalsDatapoint(
+            timestamp=datetime.now(),
+            weight_lb=0.0,
+            bp_systolic=0.0,
+            bp_diastolic=0.0,
+            heart_bpm=0,
+            notes=""
+        ))
+
+        cursor = _get_db().cursor()
+        cursor.execute("SELECT * FROM vitals")
+
+        assert type(VitalsDatapoint(
+            **cursor.fetchone()
+        )) is VitalsDatapoint
+
+    def test_can_commit_activity_datapoint(self):
+        commit(ActivityDatapoint(
+            timestamp=datetime.now(),
+            activity_type=ActivityType.WALK,
+            distance_mi=0.0,
+            duration_min=0,
+            duration_sec=0,
+            temp_f=0.0,
+            notes=""
+        ))
+
+        cursor = _get_db().cursor()
+        cursor.execute("SELECT * FROM activity")
+
+        assert type(ActivityDatapoint(
+            **cursor.fetchone()
+        )) is ActivityDatapoint
+
+    def test_can_commit_vo2_datapoint(self):
+        commit(Vo2Datapoint(
+            timestamp=datetime.now(),
+            duration_min=0,
+            duration_sec=0,
+            notes=""
+        ))
+
+        cursor = _get_db().cursor()
+        cursor.execute("SELECT * FROM vo2")
+
+        assert type(Vo2Datapoint(
+            **cursor.fetchone()
+        )) is Vo2Datapoint
+
+    def tearDown(self) -> None:
+        try:
+            unlink(DATABASE_NAME)
+        except:  # noqa
+            pass
