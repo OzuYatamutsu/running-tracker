f2e7c89a35e816fadf451655ef1b9203a17ae868
Kevin Collins <kevinc@seanco.com>
2019-08-18 17:43:38 -0400

    Add files via upload
    Add new database and schema changes.

---
+++ database/schema_running_tracker_v1.sql
@@ -0,0 +1,91 @@
+CREATE TABLE IF NOT EXISTS vitals (
+
+entry_id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+measured_on DATE NOT NULL DEFAULT (DATE('now')),
+
+weight_lb NUMERIC NOT NULL,
+
+bp_systolic NUMERIC NOT NULL,
+
+bp_diastolic NUMERIC NOT NULL,
+
+heart_bpm UNSIGNED INTEGER NOT NULL,
+
+notes TEXT(1024) NOT NULL DEFAULT ''
+
+);
+
+
+
+
+CREATE INDEX vitals_measured_on_idx ON vitals (measured_on);
+
+
+
+
+CREATE TABLE IF NOT EXISTS activity_types (
+
+name TEXT PRIMARY KEY NOT NULL
+
+);
+
+
+
+
+-- Seed activity types into table
+
+INSERT INTO activity_types (name) VALUES
+
+('run'),
+
+('run-intervals'),
+
+('walk'),
+
+('cross-training'),
+
+('cooper');
+
+
+
+
+CREATE TABLE IF NOT EXISTS activity (
+
+entry_id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+linked_to_vitals_entry INTEGER NOT NULL,
+
+activity_type TEXT NOT NULL,
+
+distance_mi NUMERIC NOT NULL,
+
+duration_min UNSIGNED INTEGER NOT NULL,
+
+duration_sec UNSIGNED INTEGER NOT NULL,
+
+feels_like_temp_f NUMERIC,
+
+steps_per_min NUMERIC,
+
+notes TEXT(1024) NOT NULL DEFAULT '',
+
+
+
+
+FOREIGN KEY (activity_type) REFERENCES activity_types(name)
+
+ON UPDATE RESTRICT
+
+ON DELETE RESTRICT,
+
+
+
+
+FOREIGN KEY (linked_to_vitals_entry) REFERENCES vitals(entry_id)
+
+ON UPDATE CASCADE
+
+ON DELETE RESTRICT
+
+);
